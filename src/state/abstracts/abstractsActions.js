53c1dab0f04e4cf8152991468298cefaad1a3a07
Jakub <psenijak@fel.cvut.cz>
2021-11-24 18:58:28 +0100

    server address changed

--- src/state/abstracts/abstractsActions.js
+++ src/state/abstracts/abstractsActions.js
@@ -1,4 +1,4 @@
-import {GET_BOOK_ABSTRACTS, GET_ABSTRACT_DETAIL, RESET_ABSTRACT_DETAIL, CUSTOM_URL_ABSTRACTS} from './abstractsTypes';
+import { GET_BOOK_ABSTRACTS, GET_ABSTRACT_DETAIL, RESET_ABSTRACT_DETAIL, CUSTOM_URL_ABSTRACTS } from './abstractsTypes';
 import { TOGGLE_LOADING, TOGGLE_ERROR } from '../appState/appStateTypes';
 import axios from 'axios';
 import { handleError } from '../../utils/error_helpers';
@@ -7,33 +7,33 @@ axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
 axios.defaults.xsrfCookieName = "csrftoken"
 // const baseUrl = "https://psenice00.pythonanywhere.com";
 // const baseUrl = "http://127.0.0.1:8000";
-const baseUrl = "http://liny-ctenar-app.eba-przuxyyc.us-west-2.elasticbeanstalk.com/";
+const baseUrl = "https://www.api.linyctenar.cz/";
 
 export const getAbstracts = (slug) => (dispatch) => {
     dispatch({ type: TOGGLE_LOADING, payload: true });
     dispatch({ type: TOGGLE_ERROR, payload: null });
     axios.get(`${baseUrl}/api/abstract/${slug}`)
-    .then((res) => {
-        dispatch({
-            type: GET_BOOK_ABSTRACTS,
-            payload: res.data,
-        });
-        dispatch({ type: TOGGLE_LOADING, payload: false });
-    })
-    .catch(error => {handleError(error, dispatch);});
+        .then((res) => {
+            dispatch({
+                type: GET_BOOK_ABSTRACTS,
+                payload: res.data,
+            });
+            dispatch({ type: TOGGLE_LOADING, payload: false });
+        })
+        .catch(error => { handleError(error, dispatch); });
 };
 export const getAbstractDetail = (slug, id) => (dispatch) => {
     dispatch({ type: TOGGLE_LOADING, payload: true });
     dispatch({ type: TOGGLE_ERROR, payload: null });
     axios.get(`${baseUrl}/api/abstract/${slug}/${id}`)
-    .then((res) => {
-        dispatch({
-            type: GET_ABSTRACT_DETAIL,
-            payload: res.data,
-        });
-        dispatch({ type: TOGGLE_LOADING, payload: false });
-    })
-    .catch(error => {handleError(error, dispatch);});
+        .then((res) => {
+            dispatch({
+                type: GET_ABSTRACT_DETAIL,
+                payload: res.data,
+            });
+            dispatch({ type: TOGGLE_LOADING, payload: false });
+        })
+        .catch(error => { handleError(error, dispatch); });
 };
 
 
@@ -41,18 +41,18 @@ export const customURLabstracts = (url) => (dispatch) => {
     dispatch({ type: TOGGLE_LOADING, payload: true });
     dispatch({ type: TOGGLE_ERROR, payload: null });
     axios.get(url)
-    .then((res) => {
-        dispatch({
-            type: CUSTOM_URL_ABSTRACTS,
-            payload: res.data,
-        });
-        dispatch({ type: TOGGLE_LOADING, payload: false });
-    })
-    .catch(error => {handleError(error, dispatch);});
+        .then((res) => {
+            dispatch({
+                type: CUSTOM_URL_ABSTRACTS,
+                payload: res.data,
+            });
+            dispatch({ type: TOGGLE_LOADING, payload: false });
+        })
+        .catch(error => { handleError(error, dispatch); });
 };
 
 export const resetAbstractDetail = () => (dispatch) => {
-    return (    dispatch({
+    return (dispatch({
         type: RESET_ABSTRACT_DETAIL,
         payload: {},
     }));
