4bb559b90a0666c40f6aaac39345608e52c5dbb4
Jakub <psenijak@fel.cvut.cz>
2021-11-26 12:11:08 +0100

    try|2

--- src/pages/writers_page/writers.js
+++ src/pages/writers_page/writers.js
@@ -67,7 +67,7 @@ const Writers = () => {
                         <div className=" pl-0 pl-xl-4 mt-3"><h2 className="text-center text-md-left mb-5 ml-0 ml-md-4 ml-lg-5  pl-0 pl-lg-5">Seznam spisovatelů</h2></div>
                         <div className="mx-auto col-12">
                             <ins class="adsbygoogle"
-                                style="display:block"
+                                style={{ display: 'block' }}
                                 data-ad-client="ca-pub-5957599795213364"
                                 data-ad-slot="2305659246"
                                 data-ad-format="auto"
